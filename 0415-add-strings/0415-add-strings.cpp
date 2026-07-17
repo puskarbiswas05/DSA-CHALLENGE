e3b9a4f719328035e362d8ff8dbf90829c34d181
Puskar Biswas <puskarbiswas0405@gmail.com>
2026-07-17 13:00:08 +0530

    Time: 0 ms (100%), Space: 9.4 MB (30.52%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+string add(string num1,string num2){
+int index1=num1.size()-1,index2=num2.size()-1;
+       string ans;
+       int carry=0;
+       int sum;
+       while(index2>=0){
+        sum=(num1[index1]-'0')+(num2[index2]-'0')+carry;
+        carry=sum/10;
+        ans+='0'+sum%10;
+        index1--;
+        index2--;
+       } 
+        while(index1>=0){
+        sum=(num1[index1]-'0')+carry;
+        carry=sum/10;
+        ans+='0'+sum%10;
+        index1--;
+       } 
+       if(carry==1)
+       ans+='1';
+       reverse(ans.begin(),ans.end());
+       return ans;
+}
+    string addStrings(string num1, string num2) {
+      if(num1.size()<num2.size())
+      return add(num2,num1);
+      else
+      return add(num1,num2);
+    }
+};
