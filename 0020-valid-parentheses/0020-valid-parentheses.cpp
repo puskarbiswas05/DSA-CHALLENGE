aa530b16f66883f6a3c19e1b2eb5ff5737b8935a
Puskar Biswas <puskarbiswas0405@gmail.com>
2026-08-24 09:57:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+     stack<char>st;
+     for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
             st.push(s[i]);
-            else{
-                if(st.size()==0)
+        }
+        else{
+            if(st.size()==0){
                 return 0;
-                else if(s[i]==')'){
-                    if(st.top()!='(')
-                    return 0;
-                    else
+            }
+            else if(s[i]==')'){
+                if(st.top()=='('){
                     st.pop();
                 }
-                else if(s[i]=='}'){
-                    if(st.top()!='{')
+                else{
                     return 0;
-                    else
+                }
+            }
+             else if(s[i]=='}'){
+                if(st.top()=='{'){
                     st.pop();
                 }
-                else if(s[i]==']'){
-                    if(st.top()!='[')
+                else{
                     return 0;
-                    else
+                }
+            }
+             else if(s[i]==']'){
+                if(st.top()=='['){
                     st.pop();
+                }
+                else{
+                    return 0;
                 }
             }
         }
-        return st.empty();
+     }
+     return st.empty();
     }
 };
