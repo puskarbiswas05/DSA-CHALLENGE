44b44fb55a19d761ebaa8b95fe26621436cb7b42
Puskar Biswas <puskarbiswas0405@gmail.com>
2026-08-30 12:41:58 +0530

    Time: 8 ms (19.13%), Space: 16.1 MB (15.32%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        string rev=s;
-        string org=s;
-        reverse(rev.begin(),rev.end());
-        int size=s.size();
-        s+='$';
-        s+=rev;
-        vector<int>lps(s.size(),0);
-        int pre=0,suf=1;
-        while(suf<s.size()){
-            if(s[pre]==s[suf]){
-                lps[suf]=pre+1;
-                pre++;
+       string org=s;
+       string rev=s;
+       int size=s.size();
+       reverse(rev.begin(),rev.end());
+       s+='$';
+       s+=rev;
+       vector<int>lps(s.size(),0);
+       int pre=0,suf=1;
+       while(suf<s.size()){
+           if(s[pre]==s[suf]){
+            lps[suf]=pre+1;
+            pre++;
+            suf++;
+
+           }
+           else{
+            if(pre==0){
+                lps[suf]=0;
                 suf++;
             }
             else{
-                if(pre==0){
-                    lps[suf]=0;
-                    suf++;
-                }
-                else{
-                    pre=lps[pre-1];
-                }
+                pre=lps[pre-1];
             }
-        }
-        return rev.substr(0,size-lps[s.size()-1])+org;
+           }
+       }
+       return rev.substr(0,size-lps[s.size()-1])+org;
+        
     }
     
 };
