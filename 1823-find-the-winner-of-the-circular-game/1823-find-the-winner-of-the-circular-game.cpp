fc224508d72cc0b5e98c2af76187b05c2c21eb47
Puskar Biswas <puskarbiswas0405@gmail.com>
2026-08-31 13:09:19 +0530

    Time: 10 ms (33.91%), Space: 8 MB (55.55%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -3,8 +3,8 @@ public:
  int winner(vector<bool>&person,int n,int index,int person_left,int k){
     if(person_left==1){
         for(int i=0;i<n;i++){
-        if(person[i]==0)
-        return i;
+            if(person[i]==0)
+            return i;
         }
     }
     int kill=(k-1)%person_left;
@@ -18,10 +18,10 @@ public:
     while(person[index]==1){
         index=(index+1)%n;
     }
-    return winner(person,n,index,person_left-1, k);
- }
+    return winner(person,n,index,person_left-1,k);
+}
     int findTheWinner(int n, int k) {
-        vector<bool>person(n,0);
-        return winner(person,n,0,n,k)+1;
+       vector<bool>person(n,0);
+       return winner(person,n,0,n,k)+1;
     }
 };
